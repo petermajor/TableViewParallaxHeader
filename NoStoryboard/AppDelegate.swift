504eae0edacf4ee8f74f209ef6898b83c8fb9b51
Peter Major <peter.major@cloudbooking.com>
2020-01-05 12:51:58 +0000

    delete unused storyboard

--- NoStoryboard/AppDelegate.swift
+++ NoStoryboard/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window?.rootViewController = ViewController(nibName: "ViewController", bundle: nil)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.rootViewController = ViewController(nibName: "ViewController", bundle: nil)
+        window!.makeKeyAndVisible()
         
         return true
     }
